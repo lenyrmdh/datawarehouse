d53267bfc25b19e90c3b14c041dba81f73498f45
lenyrmdh <setiawan.lenyramadhani@gmail.com>
2025-06-30 22:37:38 +0700

    update

--- src/database/seeders/DimWaktuSeeder.php
+++ src/database/seeders/DimWaktuSeeder.php
@@ -14,12 +14,20 @@ class DimWaktuSeeder extends Seeder
      */
     public function run(): void
     {
-        DimWaktu::create([
-            'tanggal' => '2024-06-30',
-            'bulan' => 'Juni',
-            'kuartal' => 'Q2',
-            'tahun' => 2024,
-        ]);
+        $startDate = '2024-06-01';
+        $monthsToGenerate = 20;
+
+        for ($i = 0; $i < $monthsToGenerate; $i++) {
+            $date = date_create($startDate);
+            date_add($date, date_interval_create_from_date_string("$i months"));
+
+            DimWaktu::create([
+            'tanggal' => date_format($date, 'Y-m-d'),
+            'bulan' => date_format($date, 'F'),
+            'kuartal' => 'Q' . ceil(date_format($date, 'n') / 3),
+            'tahun' => date_format($date, 'Y'),
+            ]);
+        }
 
 
     }
--- src/database/seeders/FactKeuanganSeeder.php
+++ src/database/seeders/FactKeuanganSeeder.php
@@ -20,187 +20,60 @@ class FactKeuanganSeeder extends Seeder
         $idProyek = DimProyek::pluck('id')->toArray();
         $idWilayah = DimWilayah::pluck('id')->toArray();
 
-        // 5 data laba_rugi (pengeluaran lebih besar dari pendapatan, laba_rugi tetap positif)
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 80_000_000,
-            'total_pengeluaran' => 100_000_000,
-            'laba_rugi' => abs(80_000_000 - 100_000_000),
-            'laba_untung' => 0,
-        ]);
+        // 5 data rugi (pengeluaran lebih besar dari pendapatan)
+        $rugiData = [
+            [80_000_000, 100_000_000],
+            [65_000_000, 85_000_000],
+            [55_000_000, 70_000_000],
+            [40_000_000, 60_000_000],
+            [50_000_000, 90_000_000],
+        ];
 
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 65_000_000,
-            'total_pengeluaran' => 85_000_000,
-            'laba_rugi' => abs(65_000_000 - 85_000_000),
-            'laba_untung' => 0,
-        ]);
+        foreach ($rugiData as [$pendapatan, $pengeluaran]) {
+            FactKeuangan::create([
+                'id_waktu' => $idWaktu[array_rand($idWaktu)],
+                'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
+                'id_akun' => $idAkun[array_rand($idAkun)],
+                'id_proyek' => $idProyek[array_rand($idProyek)],
+                'id_wilayah' => $idWilayah[array_rand($idWilayah)],
+                'total_pendapatan' => $pendapatan,
+                'total_pengeluaran' => $pengeluaran,
+                'laba_rugi' => abs($pendapatan - $pengeluaran),
+                'laba_untung' => 0,
+            ]);
+        }
 
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 55_000_000,
-            'total_pengeluaran' => 70_000_000,
-            'laba_rugi' => abs(55_000_000 - 70_000_000),
-            'laba_untung' => 0,
-        ]);
+        // 15 data untung (pendapatan lebih besar dari pengeluaran)
+        $untungData = [
+            [90_000_000, 60_000_000],
+            [100_000_000, 65_000_000],
+            [80_000_000, 50_000_000],
+            [120_000_000, 80_000_000],
+            [85_000_000, 55_000_000],
+            [130_000_000, 90_000_000],
+            [95_000_000, 65_000_000],
+            [115_000_000, 80_000_000],
+            [105_000_000, 75_000_000],
+            [140_000_000, 100_000_000],
+            [90_000_000, 60_000_000],
+            [125_000_000, 85_000_000],
+            [110_000_000, 70_000_000],
+            [135_000_000, 95_000_000],
+            [100_000_000, 70_000_000],
+        ];
 
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 40_000_000,
-            'total_pengeluaran' => 60_000_000,
-            'laba_rugi' => abs(40_000_000 - 60_000_000),
-            'laba_untung' => 0,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 50_000_000,
-            'total_pengeluaran' => 90_000_000,
-            'laba_rugi' => abs(50_000_000 - 90_000_000),
-            'laba_untung' => 0,
-        ]);
-
-
-        // 15 data laba_untung (positif, laba_untung = pendapatan - pengeluaran)
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 90_000_000,
-            'total_pengeluaran' => 60_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 30_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 100_000_000,
-            'total_pengeluaran' => 65_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 35_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 80_000_000,
-            'total_pengeluaran' => 50_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 30_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 120_000_000,
-            'total_pengeluaran' => 80_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 40_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 85_000_000,
-            'total_pengeluaran' => 55_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 30_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 130_000_000,
-            'total_pengeluaran' => 90_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 40_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 95_000_000,
-            'total_pengeluaran' => 65_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 30_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 115_000_000,
-            'total_pengeluaran' => 80_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 35_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 105_000_000,
-            'total_pengeluaran' => 75_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 30_000_000,
-        ]);
-
-        FactKeuangan::create([
-            'id_waktu' => $idWaktu[array_rand($idWaktu)],
-            'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
-            'id_akun' => $idAkun[array_rand($idAkun)],
-            'id_proyek' => $idProyek[array_rand($idProyek)],
-            'id_wilayah' => $idWilayah[array_rand($idWilayah)],
-            'total_pendapatan' => 140_000_000,
-            'total_pengeluaran' => 100_000_000,
-            'laba_rugi' => 0,
-            'laba_untung' => 40_000_000,
-        ]);
+        foreach ($untungData as [$pendapatan, $pengeluaran]) {
+            FactKeuangan::create([
+                'id_waktu' => $idWaktu[array_rand($idWaktu)],
+                'id_departemen' => $idDepartemen[array_rand($idDepartemen)],
+                'id_akun' => $idAkun[array_rand($idAkun)],
+                'id_proyek' => $idProyek[array_rand($idProyek)],
+                'id_wilayah' => $idWilayah[array_rand($idWilayah)],
+                'total_pendapatan' => $pendapatan,
+                'total_pengeluaran' => $pengeluaran,
+                'laba_rugi' => 0,
+                'laba_untung' => $pendapatan - $pengeluaran,
+            ]);
+        }
     }
 }
